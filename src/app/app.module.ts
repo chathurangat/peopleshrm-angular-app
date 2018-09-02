d671c279f316a011525881003ce8377820266837
Chathuranga Tennakoon <chathuranga@cyrup.sg>
2018-09-02 15:49:50 +0530

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { LeftSidebarComponent } from './left-sidebar/left-sidebar.component';
+import { TopNavbarComponent } from './top-navbar/top-navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LeftSidebarComponent,
+    TopNavbarComponent
   ],
   imports: [
     BrowserModule
